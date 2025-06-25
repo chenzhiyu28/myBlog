533d6954433eaf4035101e401bb3fa35c38ef1f5
Leopold <czy.leopold97@gmail.com>
2025-06-25 23:57:41 +1000

    add user loading page

---
+++ After/client/src/app/users/loading.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function UsersLoadingPage() {
+  return (
+    <>
+      <SkeletonList amount={6}>
+        <div className="card">
+          <div className="card-header">
+            <Skeleton short />
+          </div>
+          <div className="card-body">
+            <Skeleton short />
+            <Skeleton short />
+            <Skeleton short />
+          </div>
+          <div className="card-footer">
+            <SkeletonButton />
+          </div>
+        </div>
+      </SkeletonList>
+    </>
+  );
+}
--- After/client/src/app/users/page.tsx
+++ After/client/src/app/users/page.tsx
@@ -3,7 +3,70 @@ import React from "react";
 export default function Userpage() {
   return (
     <>
-      <h1>Users</h1>
+      <h1 className="page-title">
+        <SimpleSkeletonText resolve={userPromise}>
+          {(user) => user.name}
+        </SimpleSkeletonText>
+      </h1>
+      <div className="page-subtitle">
+        <SimpleSkeletonText resolve={userPromise}>
+          {(user) => user.email}
+        </SimpleSkeletonText>
+      </div>
+      <div>
+        <b>Company:</b>{" "}
+        <SimpleSkeletonText resolve={userPromise}>
+          {(user) => user.company.name}
+        </SimpleSkeletonText>
+      </div>
+      <div>
+        <b>Website:</b>{" "}
+        <SimpleSkeletonText resolve={userPromise}>
+          {(user) => user.website}
+        </SimpleSkeletonText>
+      </div>
+      <div>
+        <b>Address:</b>{" "}
+        <SimpleSkeletonText resolve={userPromise}>
+          {(user) => `${user.address.street} ${user.address.suite}
+        ${user.address.city} ${user.address.zipcode}`}
+        </SimpleSkeletonText>
+      </div>
+
+      <h3 className="mt-4 mb-2">Posts</h3>
+      <div className="card-grid">
+        <Suspense
+          fallback={
+            <SkeletonList amount={3}>
+              <SkeletonPostCard />
+            </SkeletonList>
+          }
+        >
+          <Await resolve={postsPromise}>
+            {(posts) =>
+              posts.map((post) => <PostCard key={post.id} {...post} />)
+            }
+          </Await>
+        </Suspense>
+      </div>
+      <h3 className="mt-4 mb-2">Todos</h3>
+      <ul>
+        <Suspense
+          fallback={
+            <SkeletonList amount={5}>
+              <li>
+                <Skeleton short />
+              </li>
+            </SkeletonList>
+          }
+        >
+          <Await resolve={todosPromise}>
+            {(todos) =>
+              todos.map((todo) => <TodoItem key={todo.id} {...todo} />)
+            }
+          </Await>
+        </Suspense>
+      </ul>
     </>
   );
 }
